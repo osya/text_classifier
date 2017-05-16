ed5a5d55ee13ca4015565178c2714d5f729babe5
Valeriy Osipov <info@vosipov.com>
2017-05-16 09:32:53 +0300

    Added KNN

--- text_classifier.py
+++ text_classifier.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, classification_report, roc_curve
 import matplotlib.pyplot as plt
 import zipfile
@@ -25,7 +26,8 @@ x_train_cv = cv.fit_transform(x_train)
 
 # model = MultinomialNB()
 # model = LogisticRegression(penalty='l2', C=1)
-model = DecisionTreeClassifier(random_state=0, max_depth=2)
+# model = DecisionTreeClassifier(random_state=0, max_depth=2)
+model = KNeighborsClassifier()
 model.fit(x_train_cv, y_train)
 
 x_test_cv = cv.transform(x_test)
@@ -35,16 +37,16 @@ print('Accuracy is %2.2f' % accuracy_score(y_test, pred))
 print('ROC AUC score is %2.2f' % roc_auc_score(y_test, pred))
 print(classification_report(y_test, pred))
 
-fpr, tpr, thresholds = roc_curve(y_test, pred)
-plt.figure()
-plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc_score(y_test, pred))
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.legend(loc='lower right')
-plt.show()
+# fpr, tpr, thresholds = roc_curve(y_test, pred)
+# plt.figure()
+# plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc_score(y_test, pred))
+# plt.plot([0, 1], [0, 1], 'k--')
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.legend(loc='lower right')
+# plt.show()
 
 # Export Decision tree in a file
 # export_graphviz(model)
